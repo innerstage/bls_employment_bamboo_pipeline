38b0f464c2245a2f48b7010da7318106df295e1a
innerstage <mbarbierif@gmail.com>
2020-09-08 16:49:07 -0300

    changes to script

--- delete_temp_and_build_model.sh
+++ delete_temp_and_build_model.sh
@@ -4,4 +4,5 @@ python etl_steps/01_scrape_available_series_list.py
 python etl_steps/02_download_available_series.py
 python etl_steps/03_concatenate_and_map_dfs.py
 python etl_steps/04_create_industry_hierarchy.py
-python etl_steps/05_Run_filling_algorithm.py
+python etl_steps/05_run_filling_algorithm.py
+python etl_steps/06_create_remaining_dimensions.py
